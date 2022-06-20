d279091bf3fb262faeac4cc1fac63cd10407d00c
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-19 19:04:05 -0500

    loss sevlet part of guess

--- src/main/java/lossServlet.java
+++ src/main/java/lossServlet.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class lossServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "lossServlet", urlPatterns = "/incorrect")
+public class lossServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        String lose = request.getParameter("lose");
+        String lose = "one";
+        request.setAttribute("lose", lose);
+        request.getRequestDispatcher("guessOutcome.jsp").forward(request, response);
+    }
 }
