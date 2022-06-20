f81047719ea1d8d62cac013fd58d040a11ce4460
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-19 19:08:52 -0500

    winservlet

--- src/main/java/winServlet.java
+++ src/main/java/winServlet.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class winServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/correct")
+public class winServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        String win = request.getParameter("win");
+        String win = ("two");
+        request.setAttribute("win", win);
+        response.getWriter().println("You should see this in the browser");
+
+        request.getRequestDispatcher("guessOutcome.jsp").forward(request, response);
+//        if(win == "one"){
+//            System.out.println("you win!");
+//        }
+    }
 }
